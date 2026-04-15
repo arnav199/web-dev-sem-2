4753dcadb2b5eb49d24bd19a826bb6b7f464ab09
Toxic_625 <arnavkapil17@gmail.com>
2026-04-15 19:25:14 +0530

    Add vite.config.js for Vite configuration setup

--- reactapp/src/article.jsx
+++ reactapp/src/article.jsx
@@ -1,4 +1,9 @@
 import React,{useState} from 'react'
+import ArtStyle from "../css/Article.module.css"
+// import styled from "styled-components"
+import styled from "@emotion/styled"
+import { css } from '@emotion/react'
+
 
 function Article(props) {
     const[count,setCount]=useState(0)
@@ -13,11 +18,25 @@ function Article(props) {
     <div>
       <h2>Article</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores nemo obcaecati a quam nihil nobis enim quod cupiditate, ipsam sunt laborum vel numquam, sint accusantium. Vitae aliquam esse culpa.</p>
-      <h3>{props.data}</h3>
+      <h3 style={{backgroundColor:"red"}}>{props.data}</h3>
       <h2>{count}</h2>
-      <button onClick={handleIncrement}>Increment</button>
+      <button className={ArtStyle.btn} onClick={handleIncrement}>Increment</button>
+      <Button>Click Me!</Button>
+      <Button red >Click Again</Button>
+      <button css={{backgroundColor:"green",color:"white",borderRadius:"10px",height:"30px",width:"150px",'&:hover':{backgroundColor:"blue"}}}>Css Prop Button</button>
+
+      <button className='bg-pink-500 text-white border-2 border-white'>Tailwind Button</button>
     </div>
   )
 }
 
 export default Article
+
+
+const Button=styled.button`
+    background-color: ${props=>props.red ? "red":"pink"};
+    color: white;
+    border-radius: 10px;
+    height: 30px;  
+    width: 100px;
+`
